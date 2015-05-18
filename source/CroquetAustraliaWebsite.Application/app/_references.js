9bdd283980286c0c63c515a89e9c56d9af19c444
Tim Murphy <tim@26tp.com>
2015-05-18 17:30:09 +1000

    Added bower package 'markdown-editpreview-ng' and demo code.

--- source/CroquetAustraliaWebsite.Application/app/_references.js
+++ source/CroquetAustraliaWebsite.Application/app/_references.js
@@ -3,5 +3,14 @@
 /// <reference path="../gulpconfig.js" />
 /// <reference path="../gulpfile.js" />
 /// <reference path="admin/files/FileUploadModule.js" />
+/// <reference path="../bower_components/bootstrap-submenu/dist/js/bootstrap-submenu.js" />
+/// <reference path="../bower_components/bootstrap-submenu/js/bootstrap-submenu.js" />
+/// <reference path="../bower_components/jquery/dist/jquery.js" />
+/// <reference path="../bower_components/ng-file-upload/ng-file-upload.js" />
+/// <reference path="../bower_components/markdown-editpreview-ng/lib/codemwnci/bootstrap-markdown.js" />
+/// <reference path="../bower_components/markdown-editpreview-ng/lib/codemwnci/markdown-editpreview-ng.js" />
+/// <reference path="../bower_components/markdown-editpreview-ng/lib/codemwnci/markdown.converter.js" />
+/// <reference path="../bower_components/markdown-editpreview-ng/lib/codemwnci/markdown.sanitizer.js" />
+/// <reference path="admin/home/addpagemodule.js" />
 // global variables.
 var angular;
---
+++ source/CroquetAustraliaWebsite.Application/app/admin/home/AddPageModule.js
@@ -0,0 +1,16 @@
+﻿(function() {
+    'use strict';
+
+    var controllerId = 'AddPageCtrl';
+    var moduleDependencies = ['codemwnci.markdown-edit-preview'];
+    var controllerDependencies = ['$scope', addPage];
+
+    angular
+        .module('app', moduleDependencies)
+        .controller(controllerId, controllerDependencies);
+
+    function addPage($scope) {
+
+        $scope.liveedit = 'I am **ready** to be edited!';
+    }
+})();
