9b4a9b8c046cd78013d391807fa4034710e3e1eb
Tim Murphy <tim@26tp.com>
2015-06-02 15:07:52 +1000

    Added Contact Us & Governance to navigation bar.

--- source/CroquetAustraliaWebsite.Application/app/Infrastructure/PublicNavigationBar.cs
+++ source/CroquetAustraliaWebsite.Application/app/Infrastructure/PublicNavigationBar.cs
@@ -8,6 +8,9 @@ namespace CroquetAustraliaWebsite.Application.App.Infrastructure
         {
             return new[]
             {
+                new NavigationItem("Contact Us", "~/governance/contact-us"),
+                new NavigationItem("Governance", 
+                    new NavigationItem("State Associations", "~/governance/state-associations")),
                 new NavigationItem("Disciplines",
                     new NavigationItem("Golf Croquet",
                         new NavigationItem("Refereeing", "~/disciplines/golf-croquet/refereeing")))
