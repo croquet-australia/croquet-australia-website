ca151d5d534d5a1c8bceae29be8d09543309426c
Tim Murphy <tim@26tp.com>
2015-06-09 13:02:46 +1000

    Updated navigation bar.

--- source/CroquetAustraliaWebsite.Application/app/Infrastructure/PublicNavigationBar.cs
+++ source/CroquetAustraliaWebsite.Application/app/Infrastructure/PublicNavigationBar.cs
@@ -8,9 +8,14 @@ namespace CroquetAustraliaWebsite.Application.App.Infrastructure
         {
             return new[]
             {
-                new NavigationItem("Contact Us", "~/governance/contact-us"),
-                new NavigationItem("Governance", 
+                new NavigationItem("Contact Us",
+                    new NavigationItem("Office & Board", "~/governance/contact-us"),
+                    new NavigationItem("Committees", "~/governance/contact-us#committees"),
+                    new NavigationItem("Appointed Officers", "~/governance/contact-us#appointed-officers"),
                     new NavigationItem("State Associations", "~/governance/state-associations")),
+                new NavigationItem("Governance",
+                    new NavigationItem("Background", "~/governance/background"),
+                    new NavigationItem("Constitution, Regulations & Policies", "~/governance/constitution-regulations-and-policies")),
                 new NavigationItem("Disciplines",
                     new NavigationItem("Golf Croquet",
                         new NavigationItem("Refereeing", "~/disciplines/golf-croquet/refereeing")))
