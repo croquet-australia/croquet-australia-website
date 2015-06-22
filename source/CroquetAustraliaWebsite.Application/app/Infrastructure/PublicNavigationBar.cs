aa685890d033e18428a98f5c9a868168d5552b09
Tim Murphy <tim@26tp.com>
2015-06-23 08:49:21 +1000

    Updated navigation bar.

--- source/CroquetAustraliaWebsite.Application/app/Infrastructure/PublicNavigationBar.cs
+++ source/CroquetAustraliaWebsite.Application/app/Infrastructure/PublicNavigationBar.cs
@@ -2,7 +2,7 @@
 
 namespace CroquetAustraliaWebsite.Application.App.Infrastructure
 {
-    public class PublicNavigationBar
+    public static class PublicNavigationBar
     {
         public static IEnumerable<NavigationItem> GetNavigationItems()
         {
@@ -15,12 +15,18 @@ namespace CroquetAustraliaWebsite.Application.App.Infrastructure
                     new NavigationItem("State Associations", "~/governance/state-associations")),
                 new NavigationItem("Governance",
                     new NavigationItem("Background", "~/governance/background"),
-                    new NavigationItem("Constitution, Regulations & Policies", "~/governance/constitution-regulations-and-policies")),
+                    new NavigationItem("Constitution, Regulations & Policies", "~/governance/constitution-regulations-and-policies"),
+                    new NavigationItem("Members", "~/governance/members")),
                 new NavigationItem("Tournaments", "~/tournaments"),
                 new NavigationItem("Disciplines",
+                    new NavigationItem("Association Croquet",
+                        new NavigationItem("Coaching", "~/disciplines/association-croquet/coaching"),
+                        new NavigationItem("Refereeing", "~/disciplines/association-croquet/refereeing")),
                     new NavigationItem("Golf Croquet",
+                        new NavigationItem("Coaching", "~/disciplines/golf-croquet/coaching"),
                         new NavigationItem("Refereeing", "~/disciplines/golf-croquet/refereeing"),
-                        new NavigationItem("Resources", "~/disciplines-golf-croquet-resources")))
+                        new NavigationItem("Resources", "~/disciplines/golf-croquet/resources")))
+
             };
         }
     }
