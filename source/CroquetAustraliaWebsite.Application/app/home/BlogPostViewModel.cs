376834b2eb020b209b228f3387ffb545a3c96672
Tim Murphy <tim@26tp.com>
2015-10-17 13:10:22 +1100

    Improve error message when BlogPost.Content markdown to html fails

--- source/CroquetAustraliaWebsite.Application/app/home/BlogPostViewModel.cs
+++ source/CroquetAustraliaWebsite.Application/app/home/BlogPostViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Web;
+using Anotar.NLog;
 using Casper.Domain.Features.BlogPosts;
 using CroquetAustraliaWebsite.Library.Content;
 
@@ -13,11 +14,35 @@ namespace CroquetAustraliaWebsite.Application.App.home
         public BlogPostViewModel(BlogPost blogPost, IMarkdownTransformer markdownTransformer)
         {
             _blogPost = blogPost;
-            _contentFactory = new Lazy<IHtmlString>(() => markdownTransformer.MarkdownToHtml(_blogPost.Content));
+            _contentFactory = new Lazy<IHtmlString>(() => MarkdownToHtml(markdownTransformer));
         }
 
-        public string Title { get { return _blogPost.Title; } }
-        public IHtmlString Content { get { return _contentFactory.Value; } }
-        public DateTimeOffset Published { get { return _blogPost.Published; } }
+        public string Title => _blogPost.Title;
+
+        public IHtmlString Content => _contentFactory.Value;
+
+        public DateTimeOffset Published => _blogPost.Published;
+
+        private IHtmlString MarkdownToHtml(IMarkdownTransformer markdownTransformer)
+        {
+            try
+            {
+                return markdownTransformer.MarkdownToHtml(_blogPost.Content);
+            }
+            catch (Exception innerException)
+            {
+                var exception = new Exception($"Could not convert content of blog post '{Title}' to HTML.",
+                    innerException);
+
+                exception.Data.Add("BlogPost.Title", _blogPost.Title);
+                exception.Data.Add("BlogPost.Content", _blogPost.Content);
+                exception.Data.Add("BlogPost.Published", _blogPost.Published);
+                exception.Data.Add("BlogPost.RelativeUri", _blogPost.RelativeUri);
+
+                LogTo.ErrorException(exception.Message, exception);
+
+                return new HtmlString("<p>Content currently not available.<p>");
+            }
+        }
     }
 }
