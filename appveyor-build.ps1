e20adef6f98dba71a7e35ef4a1514dc7bc345fa5
Tim Murphy <tim@26tp.com>
2017-05-07 06:07:40 +1000

    Fix

--- appveyor-build.ps1
+++ appveyor-build.ps1
@@ -4,11 +4,15 @@ Write-Host "Configuring powershell environment..."
 $ErrorActionPreference = "Stop"
 
 try {  
+    Main
     Write-Host "todo: Successfully completed appveyor.ps1."
-    Restore-Dependencies
 }
 catch {
     Write-Error $_.Exception
+}
+
+function Main() {
+    Restore-Dependencies
 }
 
 function Restore-Dependencies() {
