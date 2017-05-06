6bafb4e6f930a4ae7b399339d9412892e187cd51
Tim Murphy <tim@26tp.com>
2017-05-07 05:57:48 +1000

    Test error handling on AppVeyor

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,1 +1,12 @@
-Write-Host "hello from appveyor.ps1"
+Write-Host "Starting appveyor.ps1..."
+
+Write-Host "Configuring powershell environment..."
+$ErrorActionPreference = "Stop"
+
+try {  
+throw x  
+    Write-Host "Successfully completed appveyor.ps1."
+}
+catch {
+    Write-Error $_.Exception
+}
