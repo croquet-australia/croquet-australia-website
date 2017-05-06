7fb4c6eae27d426bf8aa73078a7c497ba9d8aae9
Tim Murphy <tim@26tp.com>
2017-05-07 05:59:25 +1000

    Test without throw error

--- appveyor.ps1
+++ appveyor.ps1
@@ -4,8 +4,7 @@ Write-Host "Configuring powershell environment..."
 $ErrorActionPreference = "Stop"
 
 try {  
-throw x  
-    Write-Host "Successfully completed appveyor.ps1."
+    Write-Host "todo: Successfully completed appveyor.ps1."
 }
 catch {
     Write-Error $_.Exception
