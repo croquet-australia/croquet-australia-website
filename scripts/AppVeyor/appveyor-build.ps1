1d64726467b947e6621eec7be4f7c98472d1e0cc
Tim Murphy <tim@26tp.com>
2017-05-10 05:07:44 +1000

    Restore NuGet packages

--- scripts/AppVeyor/appveyor-build.ps1
+++ scripts/AppVeyor/appveyor-build.ps1
@@ -1,16 +1,62 @@
-Write-Host "Starting appveyor-build.ps1..."
-
-Write-Host "Configuring powershell environment..."
-$ErrorActionPreference = "Stop"
-
 function Build() {
     Restore-Dependencies
 }
 
 function Restore-Dependencies() {
     Write-Host "Restoring dependencies..."
-    nuget
+
+    Install-NuGet
+    Restore-NuGetPackages
+    Restore-BowerPackages
+    Restore-NpmPackages
 }
+
+function Install-NuGet() {
+    Write-Host "Installing nuget.exe..."
+
+    if (Test-Path $settings.NuGetPath) {
+        Write-Host "NuGet already installed."
+        return
+    }
+
+    if (!(Test-Path $settings.NuGetFolder)) {
+        Write-Host "Creating NuGet folder '$($settings.NuGetFolder)'..."
+        New-Item -Path $settings.NuGetFolder -ItemType Directory
+    }
+
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile($settings.NuGetUrl, $settings.NuGetPath)
+}
+
+function Restore-NuGetPackages {
+    Run-Command { & $($settings.NuGetPath) restore }
+}
+
+function Run-Command([scriptblock] $command) {
+    & $command
+    if ($lastexitcode -ne 0) {
+        throw
+    }
+}
+
+Write-Host "Starting appveyor-build.ps1..."
+
+Write-Host "Configuring powershell environment..."
+
+$ErrorActionPreference = "Stop"
+$WarningPreference = "Continue"
+$VerbosePreference = "SilentlyContinue"
+
+$settings = @{}
+$settings.SolutionFolder = Resolve-Path $PSScriptRoot\..\..
+$settings.NuGetVersion = "v4.1.0"
+$settings.NuGetUrl = "https://dist.nuget.org/win-x86-commandline/$($settings.NuGetVersion)/nuget.exe"
+$settings.NuGetFolder = "$($settings.SolutionFolder)\.nuget\$($settings.NuGetVersion)"
+$settings.NuGetPath = "$($settings.NuGetFolder)\nuget.exe"
+
+Write-Host "Changing to solution's root folder '$solutionFolder'..."
+Push-Location $solutionFolder
+
 
 try {  
     Build
@@ -19,4 +65,6 @@ try {
 catch {
     Write-Error $_.Exception
 }
-
+finally {
+    Pop-Location
+}
--- scripts/Clean-Solution.ps1
+++ scripts/Clean-Solution.ps1
@@ -6,6 +6,7 @@ Function Main()
 
     try
     {
+        Delete-Folder .\.nuget
         Delete-Folder .\packages
         Delete-Folder .\source\CroquetAustraliaWebsite.Application\bower_packages
         Delete-Folder .\source\CroquetAustraliaWebsite.Application\node_modules
